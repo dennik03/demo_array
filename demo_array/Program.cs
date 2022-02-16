231ef809cc0f3000bcc957d8c22b1103606d7d78
Denis Ishuk <denis.ishuk@gmail.com>
2022-02-16 09:40:38 +0300

    Добавьте файлы проекта.

---
+++ demo_array/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объявим и проинициализируем массив целых чисел
+            int[] myarr = new int[] { 5, 4, 1, 7, 2, 100 , 44, 12, 4, 6 };
+            // выведем длину массива
+            Console.Write("Длина массива: ");
+            Console.WriteLine(myarr.Length);
+            Console.WriteLine("выведем 3-ий элемент массива");
+            Console.WriteLine(myarr[2]);
+            Console.WriteLine("выведем массив"); // чтобы пройти по всему массиву - нужен цикл
+            for (int i = 0; i < myarr.Length; i++)
+            {
+                Console.WriteLine(myarr[i]);
+            }
+            Console.WriteLine("выведем только четные элементы и их индексы");
+            int max = int.MinValue;
+            int index = 0;
+            for (int i = 0; i < myarr.Length; i++)
+            {
+                if (myarr[i] % 2 == 0)
+                {
+                    Console.WriteLine($"{i}-ый элемент равен {myarr[i]} ");
+                }
+                if (myarr[i]>max)
+                {
+                    max = myarr[i];
+                    index = i;
+                }
+
+            }
+
+            Console.WriteLine($"{max} - максимальный, {index} - его индекс в массиве, {index+1} - порядковый номер");
+            Console.WriteLine(myarr.Length);
+        }
+    }
+}
---
+++ demo_array/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("demo_array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("demo_array")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1f9b6534-1c25-416b-9aad-d29ea3e44977")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
